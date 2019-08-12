66459f9e7e2506ff94f0f5c672c8d3d2273635dd
mehdigoom <medhi.gay@sfr.fr>
2019-08-12 11:10:11 +0200

    init

---
+++ src/js/reducers.js
@@ -0,0 +1,12 @@
+
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import appReducer from "./reducers/app";
+
+const createRootReducer = history =>
+    combineReducers({
+        router: connectRouter(history),
+        app: appReducer,
+    });
+
+export default createRootReducer;
---
+++ src/js/store.js
@@ -0,0 +1,25 @@
+
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import createRootReducer from "./reducers";
+
+const loggerMiddleware = createLogger();
+export const history = createBrowserHistory();
+
+export default (preloadedState) => {
+    return createStore(
+        createRootReducer(history),
+        preloadedState,
+        composeWithDevTools(
+            applyMiddleware(
+                thunkMiddleware,
+                loggerMiddleware,
+                routerMiddleware(history)
+            )
+        )
+    );
+};
